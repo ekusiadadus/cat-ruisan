2b3480f7c2eca15aaf894578e305cebada6dc625
ekusiadadus <ekusiadadus@gmail.com>
2022-08-28 20:17:26 +0900

    feat: :fire: read file from cat

--- cat.c
+++ cat.c
@@ -3,10 +3,20 @@
 #include<unistd.h>
 #include<inttypes.h>
 #include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
 
 int main(int argc, char **argv){
   uint8_t buf[4096];
   ssize_t nread;
+
+  int fd = open(argv[1], O_RDONLY);
+  if (fd < 0) {
+    fprintf(stderr, "cannot open %s: %s¥n", argv[1], strerror(errno));
+    exit(1);
+  }
 
   while((nread = read(0,  buf, sizeof(buf))) > 0)
     write(1, buf, nread);
